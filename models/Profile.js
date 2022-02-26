b985e1ba2fdced01eda724f51bd4e6838326195d
berryboylb <phemmynesce4life@gmail.com>
2022-02-27 00:23:09 +0100

    User can see their profile details

--- models/Profile.js
+++ models/Profile.js
@@ -114,7 +114,6 @@ const ProfileSchema = new mongoose.Schema({
         type: Date,
         default: Date.now()
     }
-    
 });
 
 module.exports = Profile = mongoose.model('profile', ProfileSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const router = express.Router();
+//for protected routes
+const auth = require('../../middleware/auth');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 
-//@route GET api/profile
-//@description test route
-//@access public
-router.get('/', (req, res) => res.send('profile route'));
+//@route GET api/profile/me
+//@description Get current user profile
+//@access Private
+router.get('/me', auth, async (req, res) => {
+    try{
+        /*populate is to fetch things that are not in the user model 
+        but want it there eg the name, avatar*/
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', [
+            'name', 'avatar'
+        ]);
+        if(!profile) {
+            return res.status(400).json({ msg: "There is no profile for this uaer" });
+        }
+        res.json(profile);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
