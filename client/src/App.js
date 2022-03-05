99ba5aaa8c7921ee8a585d09bc895e001c3c5f37
berryboylb <phemmynesce4life@gmail.com>
2022-03-05 15:55:34 +0100

    Login, signup and landing page routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing from './components/Layout/Landing';
+import Navbar from './components/Layout/Navbar';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route exact path = "/" element={<Landing/>} />
+          <Route path = "/register" element={<Register/>} />
+          <Route path = "/login" element={<Login/>} />
+        </Routes>
+      </Router>
   );
 }
 
---
+++ client/src/components/Auth/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+      <section className='container'>
+          <div>Loginbchbshc</div>
+      </section>
+  )
+}
+
+export default Login
